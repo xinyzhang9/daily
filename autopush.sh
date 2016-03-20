27ad6c214995cec7d2b2ca8c6ea3f5f798743bca
Xinyang Zhang <Xinyang Zhang>
2016-03-20 10:14:19 -0700

    daily report update

--- autopush.sh
+++ autopush.sh
@@ -1,4 +1,5 @@
 echo "hello " >> test.txt
 git add .
 git commit -m "daily report"
-git push https://xinyzhang9:zxy17320@github.com/xinyzhang9/daily.git master
+git remote add origin https://github.com/xinyzhang9:zxy17320/daily.git
+git push -u origin master
